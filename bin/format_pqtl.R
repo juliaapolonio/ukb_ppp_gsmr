fea878f5ad7df478041f973c57d55c2890d4319c
jvfe <jvfecav@gmail.com>
2024-04-10 17:50:52 -0300

    style: Fix linter issues on R script
    Signed-off-by: jvfe <jvfecav@gmail.com>

--- bin/format_pqtl.R
+++ bin/format_pqtl.R
@@ -1,29 +1,34 @@
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly = TRUE)
 
-# Load libraries (vroom to open/write large files, dplyr and tidyr to format data)
+# Load libraries (vroom to open/write large files,
+# dplyr and tidyr to format data)
 library(vroom)
 library(dplyr)
 library(MungeSumstats)
 
-input_path = args[1]
-output_path = args[2]
-sumstats_path = args[3]
+input_path <- args[1]
+output_path <- args[2]
+sumstats_path <- args[3]
 
 dep <- vroom(sumstats_path)
 
 pqtl <- vroom(input_path)
 
 out <- pqtl %>%
-  select(c("CHROM", "GENPOS", "ALLELE0", "ALLELE1", "A1FREQ", "N", "BETA", "SE", "LOG10P"))
+  select(c("CHROM", "GENPOS", "ALLELE0", "ALLELE1",
+           "A1FREQ", "N", "BETA", "SE", "LOG10P"))
 
 out <- out %>%
-  inner_join(dep[,c("SNP", "CHR", "BP", "A1", "A2")], by = c("CHROM"="CHR", "GENPOS"="BP", "ALLELE0"="A2", "ALLELE1"= "A1")) %>%
+  inner_join(dep[, c("SNP", "CHR", "BP", "A1", "A2")],
+             by = c("CHROM" = "CHR", "GENPOS" = "BP", "ALLELE0" = "A2",
+                    "ALLELE1" = "A1")) %>%
   select(SNP, everything())
 
 out$p <- 10^(-1 * as.numeric(out$LOG10P))
 
-colnames(out) <- c("SNP", "CHR", "BP", "A2", "A1", "FREQ", "N", "BETA", "SE", "LOG10P", "P")
+colnames(out) <- c("SNP", "CHR", "BP", "A2", "A1", "FREQ",
+                   "N", "BETA", "SE", "LOG10P", "P")
 
 lift <- liftover(
   out,
@@ -40,6 +45,6 @@ lift <- liftover(
 )
 
 lift <- lift %>%
-  select(SNP, A1, A2, freq=FREQ, b=BETA, se=SE, p=P, n=N)
+  select(SNP, A1, A2, freq = FREQ, b = BETA, se = SE, p = P, n = N)
 
 vroom_write(lift, output_path)
